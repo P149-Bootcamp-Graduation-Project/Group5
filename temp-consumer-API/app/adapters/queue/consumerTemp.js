b402dc61ec0cf37793d036812efbea823281c49f
brkybzkrt <b9bozkurt959@gmail.com>
2022-01-19 22:04:26 +0300

    Edited consumers

--- temp-consumer-API/app/adapters/queue/consumerTemp.js
+++ temp-consumer-API/app/adapters/queue/consumerTemp.js
@@ -2,18 +2,22 @@ const { Kafka } = require("kafkajs");
 const converter =require("../../helper/timeConverter");
 const temperatureLogger = require("../../models/temperatureLog");
 
-//createConsumerTemperature()
+
+const kafka = new Kafka({
+  clientId: "kafka_start",
+  brokers: ["192.168.1.2:9092"],
+});
+
+const consumer = kafka.consumer({
+  groupId: "consumer_group_start",
+});
+
+
+
 
 async function createConsumerTemperature() {
   try {
-    const kafka = new Kafka({
-      clientID: "kafka_start",
-      brokers: ["192.168.1.2:9092"],
-    });
-
-    const consumer = kafka.consumer({
-      groupId: "consumer_group_start",
-    });
+   
     console.log("Temperature-sensor is connecting...");
     await consumer.connect();
     console.log("Connection is successfully...");
@@ -25,11 +29,12 @@ async function createConsumerTemperature() {
     await consumer.run({
       eachMessage: async ({message}) => {
         
-        const {DATAAAAA}= JSON.parse(message.value.toString())
-          const {id,sensor_data,time_stamp} = DATAAAAA;
-          let date = await converter(time_stamp);
-          console.log(sensor_data,date);
+        // const {DATAAAAA}= JSON.parse(message.value.toString())
+        //   const {id,sensor_data,time_stamp} = DATAAAAA;
+        //   let date = await converter(time_stamp);
+        //   console.log(sensor_data,date);
         
+        console.log(message.value.toString());
         //await temperatureLogger();
       },
     });
@@ -37,6 +42,13 @@ async function createConsumerTemperature() {
     console.log(
       "[ERROR] An error occurred while read to message from Temperature-sensor..."
     );
+
+    const errData = {
+      pwd: "./app/adapters/queue/consumerTemp.js",
+      topic: "Temperature-sensor",
+      err_func: "createConsumerTemperature",
+      content_err: error,
+    };
   }
 }
 
